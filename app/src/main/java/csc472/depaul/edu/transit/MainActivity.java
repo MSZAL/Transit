a09e97d239ad77e11160e01f01f91d16ab7f11b1
Michael <szalmichael@gmail.com>
2018-11-06 11:21:54 -0600

    NavDrawer now properly switches between different fragments

--- app/src/main/java/csc472/depaul/edu/transit/MainActivity.java
+++ app/src/main/java/csc472/depaul/edu/transit/MainActivity.java
@@ -1,38 +1,79 @@
 package csc472.depaul.edu.transit;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Transit App","it works!");
 
-        mDrawerLayout = findViewById(R.id.drawer);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            // TODO: Start map fragment instead of bus fragment
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new BusFragment()).commit();
+            navigationView.setCheckedItem(R.id.bus);
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (mToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
+        switch(menuItem.getItemId()) {
+            case R.id.map:
+                // TODO: Add map fragment
+                break;
+            case R.id.bus:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new BusFragment()).commit();
+                break;
+            case R.id.train:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TrainFragment()).commit();
+                break;
+            case R.id.metra:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MetraFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
 }
