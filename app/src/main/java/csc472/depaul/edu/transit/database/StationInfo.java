543798ac67b798ef2593f7b3e5cfb83f83a329dc
Adrian <adrianhyc@gmail.com>
2018-11-18 20:16:50 -0600

    it just works

---
+++ app/src/main/java/csc472/depaul/edu/transit/database/StationInfo.java
@@ -0,0 +1,32 @@
+package csc472.depaul.edu.transit.database;
+
+public class StationInfo {
+
+    private String name;
+    private String time;
+    private String zone;
+    private Coordinate location;
+
+    public StationInfo(String name, String time, String zone, double longitude, double latitude) {
+        this.name = name;
+        this.time = time;
+        this.zone = zone;
+        this.location = new Coordinate(longitude, latitude);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public Coordinate getLocation() {
+        return location;
+    }
+}
